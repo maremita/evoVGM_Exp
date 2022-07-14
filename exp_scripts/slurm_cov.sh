c99bd90440157f33be9c73c47d060438a2dd505a
Amine M. Remita <remita.amine@courrier.uqam.ca>
2022-07-13 22:52:48 -0400

    Copy and install deps in SLURM_TMPDIR

--- exp_scripts/slurm_cov.sh
+++ exp_scripts/slurm_cov.sh
@@ -11,14 +11,23 @@
 
 module load StdEnv/2020
 module load python/3.8
-module load imkl/2020.1.217
+module load scipy-stack
+module load imkl/2022.1.0
+
+echo Running $SLURM_JOB_NAME $SLURM_JOB_ID
+echo
 
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
 pip install --no-index --upgrade pip
 
-pip install --no-index /home/mremita/project/mremita/Thesis/Software/pyvolve
-pip install --no-index /home/mremita/project/mremita/Thesis/Software/evoVGM
+# Copy packages
+# Update here the paths for pyvolve and evovgm
+cp -a /home/mremita/project/mremita/Thesis/Software/pyvolve/. $SLURM_TMPDIR/pyvolve/
+cp -a /home/mremita/project/mremita/Thesis/Software/evoVGM/. $SLURM_TMPDIR/evoVGM/
+
+pip install --no-index $SLURM_TMPDIR/pyvolve/
+pip install --no-index $SLURM_TMPDIR/evoVGM/
 
 evovgm.py ../exp_2022_bcb/CoV_BP001AKL01_config.ini
--- exp_scripts/slurm_exps.sh
+++ exp_scripts/slurm_exps.sh
@@ -3,17 +3,25 @@
 ## SBATCH parameters are passed within the command
 
 module load StdEnv/2020
-module load python/3.9
+module load python/3.8
+module load scipy-stack
 module load imkl/2022.1.0
+
+echo Running $SLURM_JOB_NAME $SLURM_JOB_ID
+echo
 
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
 pip install --no-index --upgrade pip
 
+# Copy packages
 # Update here the paths for pyvolve and evovgm
-pip install --no-index /home/mremita/project/mremita/Thesis/Software/pyvolve
-pip install --no-index /home/mremita/project/mremita/Thesis/Software/evoVGM
+cp -a /home/mremita/project/mremita/Thesis/Software/pyvolve/. $SLURM_TMPDIR/pyvolve/
+cp -a /home/mremita/project/mremita/Thesis/Software/evoVGM/. $SLURM_TMPDIR/evoVGM/
+
+pip install --no-index $SLURM_TMPDIR/pyvolve/
+pip install --no-index $SLURM_TMPDIR/evoVGM/
 
 # Variables $PROGRAM and CONF_file are initialized with export in running script
 
